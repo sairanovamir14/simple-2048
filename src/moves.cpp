2c34ddd013062e0947c54a18484aa41ff0a04957
sairanovamir14 <sairanovamir14@gmail.com>
2026-05-12 22:03:25 +0500

    improved movement logic

--- src/moves.cpp
+++ src/moves.cpp
@@ -3,16 +3,21 @@
 // Влево
 void moveLeft()
 {
+    bool moved = false;
+
     for(int k = 0; k < 4; k++)
     {
         for(int i = 0; i < SIZE; i++)
         {
             for(int j = 1; j < SIZE; j++)
             {
-                if(board[i][j - 1] == 0)
+                if(board[i][j - 1] == 0
+                   && board[i][j] != 0)
                 {
                     board[i][j - 1] = board[i][j];
                     board[i][j] = 0;
+
+                    moved = true;
                 }
             }
         }
@@ -27,6 +32,8 @@ void moveLeft()
             {
                 board[i][j - 1] *= 2;
 
+                moved = true;
+
                 score += board[i][j - 1];
 
                 if(score > best)
@@ -39,22 +46,30 @@ void moveLeft()
         }
     }
 
-    addNumber();
+    if(moved)
+    {
+        addNumber();
+    }
 }
 
 // Вправо
 void moveRight()
 {
+    bool moved = false;
+
     for(int k = 0; k < 4; k++)
     {
         for(int i = 0; i < SIZE; i++)
         {
             for(int j = 2; j >= 0; j--)
             {
-                if(board[i][j + 1] == 0)
+                if(board[i][j + 1] == 0
+                   && board[i][j] != 0)
                 {
                     board[i][j + 1] = board[i][j];
                     board[i][j] = 0;
+
+                    moved = true;
                 }
             }
         }
@@ -69,6 +84,8 @@ void moveRight()
             {
                 board[i][j + 1] *= 2;
 
+                moved = true;
+
                 score += board[i][j + 1];
 
                 if(score > best)
@@ -81,22 +98,30 @@ void moveRight()
         }
     }
 
-    addNumber();
+    if(moved)
+    {
+        addNumber();
+    }
 }
 
 // Вверх
 void moveUp()
 {
+    bool moved = false;
+
     for(int k = 0; k < 4; k++)
     {
         for(int j = 0; j < SIZE; j++)
         {
             for(int i = 1; i < SIZE; i++)
             {
-                if(board[i - 1][j] == 0)
+                if(board[i - 1][j] == 0
+                   && board[i][j] != 0)
                 {
                     board[i - 1][j] = board[i][j];
                     board[i][j] = 0;
+
+                    moved = true;
                 }
             }
         }
@@ -111,6 +136,8 @@ void moveUp()
             {
                 board[i - 1][j] *= 2;
 
+                moved = true;
+
                 score += board[i - 1][j];
 
                 if(score > best)
@@ -123,22 +150,30 @@ void moveUp()
         }
     }
 
-    addNumber();
+    if(moved)
+    {
+        addNumber();
+    }
 }
 
 // Вниз
 void moveDown()
 {
+    bool moved = false;
+
     for(int k = 0; k < 4; k++)
     {
         for(int j = 0; j < SIZE; j++)
         {
             for(int i = 2; i >= 0; i--)
             {
-                if(board[i + 1][j] == 0)
+                if(board[i + 1][j] == 0
+                   && board[i][j] != 0)
                 {
                     board[i + 1][j] = board[i][j];
                     board[i][j] = 0;
+
+                    moved = true;
                 }
             }
         }
@@ -153,6 +188,8 @@ void moveDown()
             {
                 board[i + 1][j] *= 2;
 
+                moved = true;
+
                 score += board[i + 1][j];
 
                 if(score > best)
@@ -165,5 +202,8 @@ void moveDown()
         }
     }
 
-    addNumber();
+    if(moved)
+    {
+        addNumber();
+    }
 }
